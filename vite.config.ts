2281ec6ee1ab50fae753201570018025354d6b32
Egan Bisma <egan@hey.com>
2024-08-04 21:15:50 +0700

    YOLO

--- vite.config.ts
+++ vite.config.ts
@@ -5,46 +5,10 @@ import devServer from "@hono/vite-dev-server";
 import { pages } from "vike-cloudflare";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { cjsInterop } from "vite-plugin-cjs-interop";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  ssr: {
-    // Add problematic npm package here:
-    noExternal: [
-      "@uppy/compressor",
-      "@uppy/core",
-      "@uppy/dashboard",
-      "@uppy/drag-drop",
-      "@uppy/file-input",
-      "@uppy/progress-bar",
-      "@uppy/react",
-      "@uppy/tus",
-      "react-use",
-    ]
-  },
   plugins: [
-    cjsInterop({
-      // Add broken npm package here
-      dependencies: [
-        // Apply patch to root import:
-        //   import someImport from 'some-package'
-        "@uppy/compressor",
-        "@uppy/core",
-        "@uppy/dashboard",
-        "@uppy/drag-drop",
-        "@uppy/file-input",
-        "@uppy/progress-bar",
-        "@uppy/react",
-        "@uppy/tus",
- 
-        // Apply patch to all sub imports:
-        //   import someImport from 'some-package/path'
-        //   import someImport from 'some-package/sub/path'
-        //   ...
-        "react-use/**",
-      ]
-    }),
     nodePolyfills({
       // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
       include: ["path", "stream", "assert", "events", "zlib", "util", "buffer"],
