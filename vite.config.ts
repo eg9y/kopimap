ef1fc6a0e6825dba6c8b6594f325ee2d8c08ea3f
Egan Bisma <egan@hey.com>
2024-08-05 22:18:09 +0700

    try to use overrides

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,29 @@ import vike from 'vike/plugin';
 import devServer from "@hono/vite-dev-server";
 import { pages } from "vike-cloudflare";
 import react from "@vitejs/plugin-react";
-// Remove this import:
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
-import nodePolyfillPrefix from "./vite-plugin-node-polyfill-prefix";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfillPrefix(),
-    // Remove this plugin:
-    // nodePolyfills({
-    //   include: ["path", "stream", "assert", "events", "zlib", "util", "buffer"],
-    //   globals: {
-    //     Buffer: true,
-    //     global: true,
-    //     process: true,
-    //   },
-    // }),
+    nodePolyfills({
+      include: ["path", "stream", "assert", "events", "zlib", "util", "buffer"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      overrides: {
+        path: 'node:path',
+        stream: 'node:stream',
+        assert: 'node:assert',
+        events: 'node:events',
+        zlib: 'node:zlib',
+        util: 'node:util',
+        buffer: 'node:buffer',
+      },
+      protocolImports: true,
+    }),
     vike(),
     pages({
       server: {
@@ -47,11 +54,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['node:path', 'node:stream', 'node:assert', 'node:events', 'node:zlib', 'node:util', 'node:buffer'],
     },
   },
 });
