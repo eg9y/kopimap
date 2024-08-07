b99d0f8d95d1e564a258bc60690401154e92e34f
Egan Bisma <egan@hey.com>
2024-08-07 21:25:40 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { pages } from "vike-cloudflare";
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vike({ prerender: true }), react()],
+  plugins: [vike({ prerender: true }), react(), pages()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
