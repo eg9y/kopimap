b25878f34ea7b3516ca1c04e9e1273ac7649c08a
Egan Bisma <egan@hey.com>
2024-09-09 21:48:34 +0700

    fix url

--- pages/index/+onBeforePrerenderStart.ts
+++ pages/index/+onBeforePrerenderStart.ts
@@ -76,7 +76,7 @@ export const onBeforePrerenderStart: OnBeforePrerenderStartAsync =
       for (const cafe of allCafes) {
         const cafeSlug = createSlug(cafe.name!);
         urls.push({
-          url: `/${locale}/cafe/${cafeSlug}?place_id=${cafe.place_id}`,
+          url: `/${locale}?cafe=${cafeSlug}&place_id=${cafe.place_id}`,
           pageContext: {
             locale,
             data: {
