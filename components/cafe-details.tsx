a54dbc95dd1ff42d290aa71d37e7cc91330ce599
Egan Bisma <egan@hey.com>
2024-09-15 10:12:30 +0700

    better UX for image modal

--- components/cafe-details.tsx
+++ components/cafe-details.tsx
@@ -422,14 +422,12 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({ images }) => {
           className="fixed top-0 left-0 z-[1000] w-full h-full bg-black bg-opacity-75 flex items-center justify-center"
           onClick={closeModal}
         >
-          <div
-            className="w-[90vw] h-[90vh] max-w-4xl max-h-[80vh] relative bg-black flex items-center justify-center"
-            onClick={(e) => e.stopPropagation()}
-          >
+          <div className="w-[90vw] h-[90vh] max-w-4xl max-h-[80vh] relative bg-black flex items-center justify-center">
             <img
               src={images[selectedImageIndex]}
               alt="Expanded view"
               className="max-w-full max-h-full object-contain"
+              onClick={(e) => e.stopPropagation()}
             />
             <button
               className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2"
@@ -439,13 +437,19 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({ images }) => {
             </button>
             <button
               className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white bg-black bg-opacity-50 rounded-full p-2"
-              onClick={() => navigateImage("prev")}
+              onClick={(e) => {
+                e.stopPropagation();
+                navigateImage("prev");
+              }}
             >
               <ChevronLeftIcon size={24} />
             </button>
             <button
               className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white bg-black bg-opacity-50 rounded-full p-2"
-              onClick={() => navigateImage("next")}
+              onClick={(e) => {
+                e.stopPropagation();
+                navigateImage("next");
+              }}
             >
               <ChevronRightIcon size={24} />
             </button>
