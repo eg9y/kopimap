a95a54a2b0b24fb4d16655923cf2c330e2c33d96
Egan Bisma <egan@hey.com>
2024-11-17 11:35:26 +0700

    fix server config between envs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,12 +5,17 @@ const config: CapacitorConfig = {
 	appName: "kopimap",
 	webDir: "dist/client",
 	server: {
-		hostname: "localhost",
 		androidScheme: "https",
-		// Add these lines for development
-		url: "http://10.241.7.163:5173", // Replace YOUR_LOCAL_IP with your computer's IP address
-		cleartext: true,
+		hostname: "kopimap.com",
 	},
 };
+
+if (process.env.NODE_ENV === "development") {
+	config.server = {
+		...config.server,
+		url: "http://10.241.7.163:5173",
+		cleartext: true,
+	};
+}
 
 export default config;
